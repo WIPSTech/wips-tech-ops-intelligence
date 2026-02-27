89690e3777869bcfc38ee6dfc7296a1a894a3ccc
WIPSTech <n.t.mengdev@gmail.com>
2026-02-27 13:40:04 +0200

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // This tells the system: "Ignore lint errors during builds"
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
